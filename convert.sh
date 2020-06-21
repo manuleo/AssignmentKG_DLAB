9a163e7e2060b8ec5044dd7c67f69fd1b48bdd57
Stefano Huber <stefano.huber@gmail.com>
2020-06-21 16:12:14 +0200

    Move source code into src/

--- convert.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# USAGE: ./convert.sh notebook_name format output_name
-# Note that the format can be pdf, latex or whatever pandoc accepts.
-
-jupyter nbconvert $1 --to $2 --TagRemovePreprocessor.remove_cell_tags='["hide_cell"]' --TagRemovePreprocessor.remove_input_tags='["hide_input"]' --no-prompt --output $3
